7140268cb45d41ceed2a964135156547c6c9e623
TrashboxBobylev <trashbox.bobylev@gmail.com>
2017-01-22 16:21:15 +0700

    Ended callbacks code

--- callbacks.js
+++ callbacks.js
@@ -19,6 +19,8 @@ var CallbacksAPI = {
 	},
 	
 	invokeCallback: function(){
-		
+		for (var hook in CallbacksList[arguments[0]]){
+			CallbacksList[arguments[0]][hook].call(null, arguments[1], arguments[2], arguments[3], arguments[4], arguments[5], arguments[6], arguments[7]);
+		}
 	}
 }
